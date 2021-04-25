c41e492205a4e670be15915f7dad18ecbc1b1ec8
Shazi Awaludin <shaziawaludin23@gmail.com>
2021-04-25 23:07:55 +0700

    kerucut terpancung

--- math-bangun/src/math/bangun/MathBangun.java
+++ math-bangun/src/math/bangun/MathBangun.java
@@ -9,24 +9,29 @@ public class MathBangun {
 
     public static void main(String[] args) {
        Lingkaran ling = new Lingkaran();
-       Bola bola = new Bola();
-       TemberengBola temBol = new TemberengBola();
-//        System.out.println(ling.luas);
-        System.out.println(bola.luas);
-//        System.out.println(bola.volume);
-//        System.out.println(temBol.luas);
+//       Bola bola = new Bola();
+//       TemberengBola temBol = new TemberengBola();
+        System.out.println(ling.hitungKeliling());
+//        System.out.println(bola.luas);
+////        System.out.println(bola.volume);
+////        System.out.println(temBol.luas);
+////        System.out.println(temBol.volume1);
 //        System.out.println(temBol.volume1);
-        System.out.println(temBol.volume1);
-//        Kerucut krc = new Kerucut(12);
-//        System.out.println(krc.hitungLuas());
-//        System.out.println(krc.hitungVolume());
+////        Kerucut krc = new Kerucut(12);
+////        System.out.println(krc.hitungLuas());
+////        System.out.println(krc.hitungVolume());
+////        
+////        KeratanBola krb = new KeratanBola(3, 3, 3);
+////        System.out.println(krb.hitungLuas());
+////        System.out.println(krb.hitungKeliling());
 //        
-//        KeratanBola krb = new KeratanBola(3, 3, 3);
-//        System.out.println(krb.hitungLuas());
-//        System.out.println(krb.hitungKeliling());
-        
-        JuringBola jr = new JuringBola();
-        System.out.println(jr.luas);
+//        JuringBola jr = new JuringBola();
+//        System.out.println(jr.luas);
+
+          System.out.println("<<|  |>>");
+          KerucutTerpancung kt = new KerucutTerpancung(3, 3, 2);
+          System.out.println(kt.hitungLuas());
+          System.out.println(kt.hitungVolume());
 
     }
     
--- math-bangun/src/ruang3d/KerucutTerpancung.java
+++ math-bangun/src/ruang3d/KerucutTerpancung.java
@@ -8,10 +8,29 @@ package ruang3d;
 /**
  *
  * @author shazi awaludin
- */
+// */
 public class KerucutTerpancung extends Kerucut{
-
-    public KerucutTerpancung() {
-        super(0);
+    
+    private double t1;
+    private double t2;
+    private double ratas;
+    private double s2;
+    public KerucutTerpancung(double t1, double t2, double ratas) {
+        super(10);
+        this.t1 = t1;
+        this.t2 = t2;
+        this.ratas = ratas;
+        this.s2 = (Math.sqrt((Math.pow(super.r,2)
+                        +Math.pow(this.t2,2))));
+    }
+    
+    public double hitungLuas(){
+        return Math.PI*s2*super.r+this.ratas;
+    }
+    
+    public double hitungVolume(){
+        return 1/3.0 * Math.PI*(this.t1+this.t2)*(Math.pow(super.r, 2)
+                +(super.r*this.ratas)
+                + Math.pow(this.ratas, 2));
     }
 }
